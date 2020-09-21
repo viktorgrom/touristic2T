0cc7b083500328c93d71fcf6a4445cdd303d4454
Viktor <0673503163v@gmail.com>
2020-09-22 02:28:08 +0300

    add palace cat4-5

--- app/src/main/java/com/example/ukrainetouristic/category/PrePlaceInfoActivityZikaviMiszia.java
+++ app/src/main/java/com/example/ukrainetouristic/category/PrePlaceInfoActivityZikaviMiszia.java
@@ -25,12 +25,8 @@ public class PrePlaceInfoActivityZikaviMiszia extends AppCompatActivity {
 
         RecyclerView recyclerView = findViewById(R.id.rv_list);
         List<ItemCategory> mlist = new ArrayList<>();
-        mlist.add(new ItemCategory(41,R.drawable.test_cat4_zmievi_valu, "Змієві вали","Васильків, Київська обл.","12"));
-        mlist.add(new ItemCategory(42,R.drawable.test_cat4_zentr_europa, "Географічний центр Європи","с.Ділове, Закарпатська обл. ","12"));
-        mlist.add(new ItemCategory(43,R.drawable.test_cat1_bychach_zamok, "Бучацк","Бучач, ьська обл. 16ст.","12"));
-        mlist.add(new ItemCategory(44,R.drawable.test_cat1_pidgirzi, "Хотинська ця","Хотин, Чернів обл. 13-17ст.","12"));
-        mlist.add(new ItemCategory(45,R.drawable.test_cat1_medjib_zamok, "Меджибі́зьамок","Меджибіжил. 16ст.","12"));
-        mlist.add(new ItemCategory(46,R.drawable.test_cat1_kam_pod_zamok, "Кам'яне́ць-Пська форте́ця","Кам'яне́ць-Поді́льськ, Хмельницька обл. 16ст.","12"));
+        mlist.add(new ItemCategory(41,R.drawable.test_cat4_zmievi_valu, "Змієві вали","Васильків, Київська обл.","Це залишки оборонних споруд. Загальна довжина 2000км. Ісорики стверджують що це укріплення часів Київської Русі, але важко уявити скільки зусиль та людей потрібно для зведення такої кількості оборонних споруд, при тогочасному розвитку техніки. Також їх оборонне значення піддається сумніву, так як щоб по всій довжині розмістити війська для оборони - військо має налічувати мільйон воїнів, при тому що все населення Київської Русі в 10 ст. було близько 5млн. осіб. Вали потребують збереження та подальшого вивчення, зараз можна зустріти лише поодинокі їх частини, більша частина була знищена або розорана. "));
+        mlist.add(new ItemCategory(42,R.drawable.test_cat4_zentr_europa, "Географічний центр Європи","с.Ділове, Закарпатська обл. ","У 1885–1887 роках географи з Імператорсько-Королівського військово-географічного інституту у Відні, проводили дослідження в Закарпатській області сучасної України та встановили, що Географічний центр Європи знаходиться поряд з с. Ділове, де й встановили кам'яний знак з координатами. Варто згадати, що на цей статус претендують інші країни з певними обумовленнями. "));
 
         Adapter_recycler adapter = new Adapter_recycler(this, mlist);
         recyclerView.setAdapter(adapter);
